02f409c04c2a36d8d2fb8677cdc2ecb69bf56dcf
Ahmet Tanakol <ahmettanakol@gmail.com>
2020-05-20 16:40:58 +0200

    implementation of bst kth smallest

---
+++ src/main/java/bstkthsmallest/Solution.java
@@ -0,0 +1,71 @@
+package bstkthsmallest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+ */
+class Solution {
+    List<Integer> arrList = new ArrayList<>();
+
+    public int kthSmallest(TreeNode root, int k) {
+        helperFunc(root);
+        int index = arrList.size() - k;
+        return arrList.get(index);
+    }
+
+    public void helperFunc(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        helperFunc(node.right);
+        arrList.add(node.val);
+        helperFunc(node.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
